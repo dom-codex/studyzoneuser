3b2012f791ef6fc0b3041a71de1ec60da926db0d
dominic <dominicwest7@gmail.com>
2022-01-24 15:00:51 -0800

    update code

--- helpers/loginValidator.js
+++ helpers/loginValidator.js
@@ -16,11 +16,22 @@ module.exports = async (req, res, next) => {
       message: "account does  not exist",
     });
   }
+  if(user.isBlocked){
+    return res.status(401).json({
+      code:401,
+      message:"account suspended contact support",
+     
+    })
+  }
   //2. check if account is validated and logged in
   if (!user.isActivated) {
     return res.status(401).json({
       code: 401,
       message: "account is not activated",
+       email:user.email,
+      uid:user.uid,
+      phone:user.phone,
+      name:user.name
     });
   }
 
@@ -39,12 +50,7 @@ module.exports = async (req, res, next) => {
       message: "invalid email or password",
     });
   }
-  if(user.isBlocked){
-    return res.status(401).json({
-      code:401,
-      message:"account suspended contact support"
-    })
-  }
+
   req.canLogin = true;
   next();
 };
