6bb653f3ba67fa1d9709b9b7c639cf5dc00fd78e
dominic <dominicwest7@gmail.com>
2022-01-25 13:46:23 -0800

    update code

--- helpers/loginValidator.js
+++ helpers/loginValidator.js
@@ -2,13 +2,13 @@ const userDb = require("../models/user");
 const bcrypt = require("bcrypt");
 const { Op } = require("sequelize");
 module.exports = async (req, res, next) => {
-  const { email, password,deviceId} = req.body;
+  const { email, password, deviceId } = req.body;
   //1. check if usr exist
   const user = await userDb.findOne({
     where: {
-      [Op.and]: [{ email: email },{deviceId:deviceId}],
+      [Op.and]: [{ email: email }, { deviceId: deviceId }],
     },
-    attributes: ["id", "email", "isLoggedIn", "password", "isActivated","isBlocked","deviceId"],
+    attributes: ["id", "email", "isLoggedIn", "password", "isActivated", "isBlocked", "deviceId","phone","name","uid"],
   });
   if (!user) {
     return res.status(404).json({
@@ -16,11 +16,11 @@ module.exports = async (req, res, next) => {
       message: "account does  not exist",
     });
   }
-  if(user.isBlocked){
+  if (user.isBlocked) {
     return res.status(401).json({
-      code:401,
-      message:"account suspended contact support",
-     
+      code: 401,
+      message: "account suspended contact support",
+
     })
   }
   //2. check if account is validated and logged in
@@ -28,14 +28,14 @@ module.exports = async (req, res, next) => {
     return res.status(401).json({
       code: 401,
       message: "account is not activated",
-       email:user.email,
-      uid:user.uid,
-      phone:user.phone,
-      name:user.name
+      email: user.dataValues.email,
+      uid: user.dataValues.uid,
+      phone: user.dataValues.phone,
+      name: user.dataValues.name
     });
   }
 
-  if (deviceId!=user.deviceId) {
+  if (deviceId != user.deviceId) {
     return res.status(400).json({
       code: 400,
       message: "account is already logged in from another device",
